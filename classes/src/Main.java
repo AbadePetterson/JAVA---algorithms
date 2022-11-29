ca11c5f82df9cba64400fdd9e08dcda272267414
Petterson Abade <abadepetterson@gmail.com>
2022-11-29 06:50:12 -0300

    IMC usando classes e métodos

---
+++ classes/src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pessoa objetoPessoa = new Pessoa(); //criação do objeto
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o peso da pessoa");
+        objetoPessoa.peso = leitor.nextFloat();
+        System.out.println("Digite a altura da pessoa");
+        objetoPessoa.altura = leitor.nextFloat();
+
+        System.out.println("IMC = " + objetoPessoa.calcularIMC()); //executar ações do objeto
+    }
+}
---
+++ classes/src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+    //Atributos
+    float peso;
+    float altura;
+
+    public float calcularIMC() {
+        float imc = peso / (altura * altura);
+        return imc;
+    }
+}
